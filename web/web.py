da5852182596167cfb7a5d56e03153d36c8034d5
Fadi Hanna Al-Kass <eliya.3@wright.edu>
2016-11-06 17:57:07 -0500

    .

--- web/web.py
+++ web/web.py
@@ -8,3 +8,7 @@ webApp = Flask("", template_folder='templates', static_folder='static')
 @webApp.route("/")
 def index():
     return render_template("index.html"), 200
+
+@webApp.errorhandler(404)
+def page_not_found(e):
+    return render_template("index.html"), 200
