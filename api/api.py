2e889945e7f3eef8b7b60eb6e41ad6469dbb53eb
Fadi Hanna Al-Kass <eliya.3@wright.edu>
2016-11-07 22:19:20 -0500

    .

--- api/api.py
+++ api/api.py
@@ -16,22 +16,21 @@ api.add_resource(SubmitHandler, "/submit/")
 class Query(Resource):
     def get(self):
         shortuuid.set_alphabet("1234567890")
-        dummyJsonList = []
-        for i in range(5):
-            dummyJson =\
-            {
-                "_id" : shortuuid.uuid()[0:10],
-                "platform" : ("Android", "iOS")[randint(0, 1)],
-                "release" : ("1.0.0", "1.7.0", "2.0.2", "3.5.2", "7.1.0")[randint(0, 4)],
-                "exception_type" : ("IOException", "NetworkAdapterException", "IntentException", "InvalidParameterException")[randint(0, 3)],
-                "class" : ("MainActivity", "MediaHandler", "Camera")[randint(0, 2)],
-                "function" : ("onCreate", "onStart", "onPause", "onResume", "onDestroy", "loadFragment", "loadImage", "startCamera", "sendRawRecording")[randint(0, 8)],
-                "line_num" : randint(0, 25),
-                "num_reports" : randint(0, 100),
-                "status" : ("Reported", "Resolved", "Review")[randint(0, 2)]
-            }
-            dummyJsonList.append(dummyJson)
-        return dummyJsonList
+        data = {}
+        data["data"] = []
+        for i in range(200):
+            tempData = {}
+            tempData["_id"] = shortuuid.uuid()[:10]
+            tempData["platform"] = ("Android", "iOS", "Windows Phone")[randint(0, 2)]
+            tempData["release"] = ("1.0.0", "1.0.2", "2.1.2", "3.2.6", "4.0.0")[randint(0, 4)]
+            tempData["exception_type"] = ("IntentException", "NetworkUnreachableException", "InvalidParameterException", "CameraCrashException")[randint(0, 3)]
+            tempData["class"] = ("MainActivity", "CameraLoader", "MediaHandler", "MediaUploader", "Message", "VoiceCallHandler")[randint(0, 5)]
+            tempData["function"] = ("onCreate", "onDestroy", "sendText", "sendVideo", "sendVoice", "sendImage", "onResume")[randint(0, 6)]
+            tempData["line_num"] = range(100)[randint(0, 99)]
+            tempData["num_reports"] = range(100)[randint(1, 99)]
+            tempData["status"] = ("Reported", "Resolved", "Reviewed")[randint(0, 2)]
+            data["data"].append(tempData)
+        return data
 api.add_resource(Query, "/query/")
 
 def run(host, port, debug, rdb):
--- static/js/main.js
+++ static/js/main.js
@@ -1,52 +1,16 @@
 $(document).ready(function() {
-    $.getJSON("/query/", data, success);
-
-    function data(_data) {
-        if (_data.length > 0) {
-            $(".loading").remove();
-            $(".table").append('<tbody>');
-            _data.forEach(handleDefectItem);
-            $(".table").append('</tbody>');
-        } else {
-            $(".loading").children().children(".loadingImage").hide();
-            $(".loading").children().children().html("There are no defects");
-        }
-    }
-
-    function success(data) {
-        /* nothing to be done in the meantime */
-    }
-
-    function handleDefectItem(defect) {
-        $("tbody").append(
-            '<tr class=' + getClassTypeFromStatus(defect.status) + '>' +
-            '<td>' + defect._id + '</td>' +
-            '<td>' + defect.platform + '</td>' +
-            '<td>' + defect.release + '</td>' +
-            '<td>' + defect.exception_type + '</td>' +
-            '<td>' + defect.class + '</td>' +
-            '<td>' + defect.function+'</td>' +
-            '<td>' + defect.line_num + '</td>' +
-            '<td>' + defect.num_reports + '</td>' +
-            '<td>' + defect.status + '</td>' +
-            '/tr>'
-        );
-    }
-
-    function getClassTypeFromStatus(status) {
-        switch (status) {
-            case "Reported":
-                return "danger";
-            case "Resolved":
-                return "success";
-            default:
-                return "info";
-        }
-    }
-
-    $(".ReloadButton").click(function() {
-        $(this).fadeOut(function() {
-            /*more specific search*/
-        });
-    });
-});
+    $('#defects').DataTable( {
+        "ajax": "/query/",
+        "columns": [
+            { "data": "_id" },
+            { "data": "platform" },
+            { "data": "release" },
+            { "data": "exception_type" },
+            { "data": "class" },
+            { "data": "function" },
+            { "data": "line_num" },
+            { "data": "num_reports" },
+            { "data": "status" },
+        ]
+    } );
+} );
